6a6522fefabcf442c76fed6d19e63172bdcb7663
Jaycie Ewald <jaycie.ewald@outlook.com>
2023-11-24 19:23:29 -0600

    brender whitespace

--- c/brender/brprogram.c
+++ c/brender/brprogram.c
@@ -23,8 +23,8 @@ static br_program_event_cbfn *event_cbfn = NULL;
 /* begin hook */
 void _BrBeginHook(void)
 {
-    BrDevAddStatic(NULL, BrDrv1SDL2Begin, NULL);
-    BrDevAddStatic(NULL, BrDrv1GLBegin, NULL);
+	BrDevAddStatic(NULL, BrDrv1SDL2Begin, NULL);
+	BrDevAddStatic(NULL, BrDrv1GLBegin, NULL);
 }
 
 /* end hook */
@@ -36,197 +36,197 @@ void _BrEndHook(void)
 /* create scene */
 static br_error BrProgramCreateScene(void)
 {
-    br_error r = BRE_OK;
-    br_camera *camdata;
+	br_error r = BRE_OK;
+	br_camera *camdata;
 
-    world = BrActorAllocate(BR_ACTOR_NONE, NULL);
-    order_table = BrZsOrderTableAllocate(1024, BR_ORDER_TABLE_LEAVE_BOUNDS | BR_ORDER_TABLE_BUCKET_SORT, BR_SORT_AVERAGE);
-    BrZsActorOrderTableSet(world, order_table);
+	world = BrActorAllocate(BR_ACTOR_NONE, NULL);
+	order_table = BrZsOrderTableAllocate(1024, BR_ORDER_TABLE_LEAVE_BOUNDS | BR_ORDER_TABLE_BUCKET_SORT, BR_SORT_AVERAGE);
+	BrZsActorOrderTableSet(world, order_table);
 
-    /* add camera */
-    camera = BrActorAdd(world, BrActorAllocate(BR_ACTOR_CAMERA, NULL));
-    camdata = camera->type_data;
-    camdata->field_of_view = BR_ANGLE_DEG(60);
-    camdata->hither_z = BR_SCALAR(0.1);
-    camdata->yon_z = BR_SCALAR(2048);
-    camdata->aspect = BR_DIV(BR_SCALAR(screen->width), BR_SCALAR(screen->height));
+	/* add camera */
+	camera = BrActorAdd(world, BrActorAllocate(BR_ACTOR_CAMERA, NULL));
+	camdata = camera->type_data;
+	camdata->field_of_view = BR_ANGLE_DEG(60);
+	camdata->hither_z = BR_SCALAR(0.1);
+	camdata->yon_z = BR_SCALAR(2048);
+	camdata->aspect = BR_DIV(BR_SCALAR(screen->width), BR_SCALAR(screen->height));
 
-    order_table->min_z = camdata->hither_z;
-    order_table->max_z = camdata->yon_z;
+	order_table->min_z = camdata->hither_z;
+	order_table->max_z = camdata->yon_z;
 
-    return r;
+	return r;
 }
 
 void BrProgramInitHook(br_program_init_cbfn *hook)
 {
-    if (hook) init_cbfn = hook;
+	if (hook) init_cbfn = hook;
 }
 
 void BrProgramQuitHook(br_program_quit_cbfn *hook)
 {
-    if (hook) quit_cbfn = hook;
+	if (hook) quit_cbfn = hook;
 }
 
 void BrProgramRenderHook(br_program_render_cbfn *hook)
 {
-    if (hook) render_cbfn = hook;
+	if (hook) render_cbfn = hook;
 }
 
 void BrProgramEventHook(br_program_event_cbfn *hook)
 {
-    if (hook) event_cbfn = hook;
+	if (hook) event_cbfn = hook;
 }
 
 /* initialize program */
 br_error BrProgramInit(const char *title, int w, int h)
 {
-    br_error r = BRE_OK;
+	br_error r = BRE_OK;
 
-    /* x11 hint */
-    SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0");
+	/* x11 hint */
+	SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0");
 
-    /* begin brender */
-    BrBegin();
-    BrModelFindHook(BrModelFindFailedLoad);
-    BrMapFindHook(BrMapFindFailedLoad);
-    BrMaterialFindHook(BrMaterialFindFailedLoad);
+	/* begin brender */
+	BrBegin();
+	BrModelFindHook(BrModelFindFailedLoad);
+	BrMapFindHook(BrMapFindFailedLoad);
+	BrMaterialFindHook(BrMaterialFindFailedLoad);
 
-    /* begin device */
-    r = BrDevBeginVar(&screen, "SDL2",
-                      BRT_WIDTH_I32,        w,
-                      BRT_HEIGHT_I32,       h,
-                      BRT_PIXEL_TYPE_U8,    BR_PMT_RGB_888,
-                      BRT_WINDOW_NAME_CSTR, title,
-                      BRT_HIDPI_B,          BR_TRUE,
-                      BRT_RESIZABLE_B,      BR_TRUE,
-                      BRT_OPENGL_B,         BR_TRUE,
-                      BR_NULL_TOKEN);
+	/* begin device */
+	r = BrDevBeginVar(&screen, "SDL2",
+					BRT_WIDTH_I32, w,
+					BRT_HEIGHT_I32, h,
+					BRT_PIXEL_TYPE_U8, BR_PMT_RGB_888,
+					BRT_WINDOW_NAME_CSTR, title,
+					BRT_HIDPI_B, BR_TRUE,
+					BRT_RESIZABLE_B, BR_TRUE,
+					BRT_OPENGL_B, BR_TRUE,
+					BR_NULL_TOKEN);
 
-    if (r != BRE_OK) return r;
+	if (r != BRE_OK) return r;
 
-    /* fix up origin */
-    screen->origin_x = (br_int_16)(screen->width / 2);
-    screen->origin_y = (br_int_16)(screen->height / 2);
+	/* fix up origin */
+	screen->origin_x = (br_int_16)(screen->width / 2);
+	screen->origin_y = (br_int_16)(screen->height / 2);
 
-    /* setup and resize buffers */
-    r = BrSDLUtilResizeBuffers(screen, &colour, &depth);
-    if (r != BRE_OK) return r;
+	/* setup and resize buffers */
+	r = BrSDLUtilResizeBuffers(screen, &colour, &depth);
+	if (r != BRE_OK) return r;
 
-    /* begin renderer */
-    BrRendererBegin(colour, NULL, NULL, primitive_heap, sizeof(primitive_heap));
+	/* begin renderer */
+	BrRendererBegin(colour, NULL, NULL, primitive_heap, sizeof(primitive_heap));
 
-    /* create scene */
-    BrProgramCreateScene();
+	/* create scene */
+	BrProgramCreateScene();
 
-    /* do user hook */
-    if (init_cbfn)
-        r = init_cbfn(world, camera, screen, colour, depth);
+	/* do user hook */
+	if (init_cbfn)
+		r = init_cbfn(world, camera, screen, colour, depth);
 
-    return r;
+	return r;
 }
 
 /* do main loop */
 br_error BrProgramLoop(void)
 {
-    br_error r = BRE_OK;
-    br_boolean running = BR_TRUE;
-    SDL_Event event;
+	br_error r = BRE_OK;
+	br_boolean running = BR_TRUE;
+	SDL_Event event;
 
-    /* main loop */
-    while (running == BR_TRUE && r == BRE_OK)
-    {
-        /* event poll loop */
-        while (SDL_PollEvent(&event) > 0)
-        {
-            /* handle events */
-            switch (event.type)
-            {
-                /* quit */
-                case SDL_QUIT:
-                {
-                    running = BR_FALSE;
-                    break;
-                }
+	/* main loop */
+	while (running == BR_TRUE && r == BRE_OK)
+	{
+		/* event poll loop */
+		while (SDL_PollEvent(&event) > 0)
+		{
+			/* handle events */
+			switch (event.type)
+			{
+				/* quit */
+				case SDL_QUIT:
+				{
+					running = BR_FALSE;
+					break;
+				}
 
-                /* window */
-                case SDL_WINDOWEVENT:
-                {
-                    r = BrPixelmapHandleWindowEvent(screen, &event.window);
+				/* window */
+				case SDL_WINDOWEVENT:
+				{
+					r = BrPixelmapHandleWindowEvent(screen, &event.window);
 
-                    /* handle window events */
-                    switch (event.window.event)
-                    {
-                        /* window resize */
-                        case SDL_WINDOWEVENT_SIZE_CHANGED:
-                        {
-                            /* update origin */
-                            screen->origin_x = (br_int_16)(screen->width / 2);
-                            screen->origin_y = (br_int_16)(screen->height / 2);
+					/* handle window events */
+					switch (event.window.event)
+					{
+						/* window resize */
+						case SDL_WINDOWEVENT_SIZE_CHANGED:
+						{
+							/* update origin */
+							screen->origin_x = (br_int_16)(screen->width / 2);
+							screen->origin_y = (br_int_16)(screen->height / 2);
 
-                            /* resize buffers */
-                            r = BrSDLUtilResizeBuffers(screen, &colour, &depth);
+							/* resize buffers */
+							r = BrSDLUtilResizeBuffers(screen, &colour, &depth);
 
-                            /* update camera aspect ratio */
-                            ((br_camera *)camera->type_data)->aspect = BR_DIV(BR_SCALAR(screen->width),
-                                                                              BR_SCALAR(screen->height));
-                            break;
-                        }
+							/* update camera aspect ratio */
+							((br_camera *)camera->type_data)->aspect = BR_DIV(BR_SCALAR(screen->width), BR_SCALAR(screen->height));
 
-                        /* not handled */
-                        default:
-                        {
-                            break;
-                        }
-                    }
+							break;
+						}
 
-                    break;
-                }
+						/* not handled */
+						default:
+						{
+							break;
+						}
+					}
 
-                /* not handled */
-                default:
-                {
-                    break;
-                }
-            }
+					break;
+				}
 
-            /* do user hook */
-            if (event_cbfn)
-                r = event_cbfn(&event);
-        }
+				/* not handled */
+				default:
+				{
+					break;
+				}
+			}
 
-        /* begin render */
-        BrRendererFrameBegin();
+			/* do user hook */
+			if (event_cbfn)
+				r = event_cbfn(&event);
+		}
 
-        /* do user hook */
-        if (render_cbfn)
-            r = render_cbfn();
+		/* begin render */
+		BrRendererFrameBegin();
 
-        /* end render */
-        BrRendererFrameEnd();
+		/* do user hook */
+		if (render_cbfn)
+			r = render_cbfn();
 
-        /* push to screen  */
-        BrPixelmapDoubleBuffer(screen, colour);
-    }
+		/* end render */
+		BrRendererFrameEnd();
 
-    return r;
+		/* push to screen */
+		BrPixelmapDoubleBuffer(screen, colour);
+	}
+
+	return r;
 }
 
 /* shutdown program */
 void BrProgramQuit(void)
 {
-    /* do user hook */
-    if (quit_cbfn)
-        quit_cbfn();
+	/* do user hook */
+	if (quit_cbfn)
+		quit_cbfn();
 
-    /* shutdown renderer */
-    BrRendererEnd();
+	/* shutdown renderer */
+	BrRendererEnd();
 
-    /* free pixelmaps */
-    if (depth) BrPixelmapFree(depth);
-    if (colour) BrPixelmapFree(colour);
-    if (screen) BrPixelmapFree(screen);
+	/* free pixelmaps */
+	if (depth) BrPixelmapFree(depth);
+	if (colour) BrPixelmapFree(colour);
+	if (screen) BrPixelmapFree(screen);
 
-    /* end BRender */
-    BrEnd();
+	/* end BRender */
+	BrEnd();
 }
 
--- c/brender/tile.c
+++ c/brender/tile.c
@@ -39,91 +39,91 @@ static br_uint_8 map[MAP_HEIGHT][MAP_WIDTH] = {
 };
 
 static struct {
-    br_pixelmap *screen;
-    br_pixelmap *colour;
-    br_pixelmap *depth;
-    br_actor *world;
-    br_actor *camera;
-    br_actor *camera_bbox;
-    br_actor *level;
-    br_uint_64 ticks_then;
-    br_uint_64 ticks_now;
-    const Uint8 *keys;
+	br_pixelmap *screen;
+	br_pixelmap *colour;
+	br_pixelmap *depth;
+	br_actor *world;
+	br_actor *camera;
+	br_actor *camera_bbox;
+	br_actor *level;
+	br_uint_64 ticks_then;
+	br_uint_64 ticks_now;
+	const Uint8 *keys;
 } prog;
 
 br_bounds get_absolute_bounds(br_actor *a)
 {
-    br_bounds r = a->model->bounds;
+	br_bounds r = a->model->bounds;
 
-    r.min.v[0] += a->t.t.translate.t.v[0];
-    r.min.v[1] += a->t.t.translate.t.v[1];
-    r.min.v[2] += a->t.t.translate.t.v[2];
+	r.min.v[0] += a->t.t.translate.t.v[0];
+	r.min.v[1] += a->t.t.translate.t.v[1];
+	r.min.v[2] += a->t.t.translate.t.v[2];
 
-    r.max.v[0] += a->t.t.translate.t.v[0];
-    r.max.v[1] += a->t.t.translate.t.v[1];
-    r.max.v[2] += a->t.t.translate.t.v[2];
+	r.max.v[0] += a->t.t.translate.t.v[0];
+	r.max.v[1] += a->t.t.translate.t.v[1];
+	r.max.v[2] += a->t.t.translate.t.v[2];
 
-    return r;
+	return r;
 }
 
 /* swept aabb */
 br_scalar aabb_swept(br_actor *a, br_actor *b, br_vector3 *normal)
 {
-    return BR_SCALAR(0);
+	return BR_SCALAR(0);
 }
 
 /* aabb */
 br_boolean actor_overlapping(br_actor *a, br_actor *b)
 {
-    br_bounds a_abs = get_absolute_bounds(a);
-    br_bounds b_abs = get_absolute_bounds(b);
+	br_bounds a_abs = get_absolute_bounds(a);
+	br_bounds b_abs = get_absolute_bounds(b);
 
-    /* return true if colliding */
-    return !(a_abs.max.v[0] < b_abs.min.v[0] || a_abs.min.v[0] > b_abs.max.v[0] ||
-            a_abs.max.v[1] < b_abs.min.v[1] || a_abs.min.v[1] > b_abs.max.v[1] ||
-            a_abs.max.v[2] < b_abs.min.v[2] || a_abs.min.v[2] > b_abs.max.v[2]);
+	/* return true if colliding */
+	return !(a_abs.max.v[0] < b_abs.min.v[0] || a_abs.min.v[0] > b_abs.max.v[0] ||
+			a_abs.max.v[1] < b_abs.min.v[1] || a_abs.min.v[1] > b_abs.max.v[1] ||
+			a_abs.max.v[2] < b_abs.min.v[2] || a_abs.min.v[2] > b_abs.max.v[2]);
 }
 
 br_error prog_init(br_actor *world, br_actor *camera, br_pixelmap *screen, br_pixelmap *colour, br_pixelmap *depth)
 {
-    memset(&prog, 0, sizeof(prog));
+	memset(&prog, 0, sizeof(prog));
 
-    /* store a copy for ourselves */
-    prog.world = world;
-    prog.camera = camera;
-    prog.screen = screen;
-    prog.colour = colour;
-    prog.depth = depth;
+	/* store a copy for ourselves */
+	prog.world = world;
+	prog.camera = camera;
+	prog.screen = screen;
+	prog.colour = colour;
+	prog.depth = depth;
 
-    /* move camera */
-    BrMatrix34Translate(&prog.camera->t.t.mat, BR_SCALAR(CAMERA_START_X), BR_SCALAR(0), BR_SCALAR(CAMERA_START_Y));
+	/* move camera */
+	BrMatrix34Translate(&prog.camera->t.t.mat, BR_SCALAR(CAMERA_START_X), BR_SCALAR(0), BR_SCALAR(CAMERA_START_Y));
 
-    prog.camera_bbox = BrActorAdd(prog.world, BrActorAllocate(BR_ACTOR_MODEL, NULL));
-    prog.camera_bbox->model = BrModelFind("cube.dat");
-    prog.camera_bbox->material = BrMaterialFind("checkerboard.mat");
+	prog.camera_bbox = BrActorAdd(prog.world, BrActorAllocate(BR_ACTOR_MODEL, NULL));
+	prog.camera_bbox->model = BrModelFind("cube.dat");
+	prog.camera_bbox->material = BrMaterialFind("checkerboard.mat");
 
-    /* start counting time */
-    prog.ticks_then = SDL_GetTicks64();
+	/* start counting time */
+	prog.ticks_then = SDL_GetTicks64();
 
-    /* create level scene */
-    prog.level = BrActorAdd(prog.world, BrActorAllocate(BR_ACTOR_NONE, NULL));
-    for (int y = 0; y < MAP_HEIGHT; y++)
-    {
-        for (int x = 0; x < MAP_WIDTH; x++)
-        {
-            if (map[y][x] > 0)
-            {
-                br_actor *cube = BrActorAdd(prog.level, BrActorAllocate(BR_ACTOR_MODEL, NULL));
+	/* create level scene */
+	prog.level = BrActorAdd(prog.world, BrActorAllocate(BR_ACTOR_NONE, NULL));
+	for (int y = 0; y < MAP_HEIGHT; y++)
+	{
+		for (int x = 0; x < MAP_WIDTH; x++)
+		{
+			if (map[y][x] > 0)
+			{
+				br_actor *cube = BrActorAdd(prog.level, BrActorAllocate(BR_ACTOR_MODEL, NULL));
 
-                cube->model = BrModelFind("cube.dat");
-                cube->material = BrMaterialFind("checkerboard.mat");
+				cube->model = BrModelFind("cube.dat");
+				cube->material = BrMaterialFind("checkerboard.mat");
 
-                BrMatrix34Translate(&cube->t.t.mat, BR_SCALAR(x), BR_SCALAR(0), BR_SCALAR(y));
-           }
-        }
-    }
+				BrMatrix34Translate(&cube->t.t.mat, BR_SCALAR(x), BR_SCALAR(0), BR_SCALAR(y));
+			}
+		}
+	}
 
-    return BRE_OK;
+	return BRE_OK;
 }
 
 void prog_quit(void)
@@ -133,79 +133,79 @@ void prog_quit(void)
 
 br_error prog_render(void)
 {
-    /* get deltatime */
-    float dt;
-    prog.ticks_now = SDL_GetTicks64();
-    dt = (float)(prog.ticks_now - prog.ticks_then) / 1000.0f;
-    prog.ticks_then = prog.ticks_now;
+	/* get deltatime */
+	float dt;
+	prog.ticks_now = SDL_GetTicks64();
+	dt = (float)(prog.ticks_now - prog.ticks_then) / 1000.0f;
+	prog.ticks_then = prog.ticks_now;
 
-    /* get key state */
-    prog.keys = SDL_GetKeyboardState(NULL);
+	/* get key state */
+	prog.keys = SDL_GetKeyboardState(NULL);
 
-    /* turning left and right  */
-    if (prog.keys[SDL_SCANCODE_LEFT])
-        BrMatrix34PreRotateY(&prog.camera->t.t.mat, BR_ANGLE_DEG(BR_SCALAR(80) * BR_SCALAR(dt)));
-    if (prog.keys[SDL_SCANCODE_RIGHT])
-        BrMatrix34PreRotateY(&prog.camera->t.t.mat, BR_ANGLE_DEG(BR_SCALAR(-80) * BR_SCALAR(dt)));
+	/* turning left and right */
+	if (prog.keys[SDL_SCANCODE_LEFT])
+		BrMatrix34PreRotateY(&prog.camera->t.t.mat, BR_ANGLE_DEG(BR_SCALAR(80) * BR_SCALAR(dt)));
+	if (prog.keys[SDL_SCANCODE_RIGHT])
+		BrMatrix34PreRotateY(&prog.camera->t.t.mat, BR_ANGLE_DEG(BR_SCALAR(-80) * BR_SCALAR(dt)));
 
-    /* moving forward and back */
-    if (prog.keys[SDL_SCANCODE_UP] || prog.keys[SDL_SCANCODE_W])
-        BrMatrix34PreTranslate(&prog.camera->t.t.mat, BR_SCALAR(0), BR_SCALAR(0), BR_SCALAR(-8) * BR_SCALAR(dt));
-    if (prog.keys[SDL_SCANCODE_DOWN] || prog.keys[SDL_SCANCODE_S])
-        BrMatrix34PreTranslate(&prog.camera->t.t.mat, BR_SCALAR(0), BR_SCALAR(0), BR_SCALAR(8) * BR_SCALAR(dt));
+	/* moving forward and back */
+	if (prog.keys[SDL_SCANCODE_UP] || prog.keys[SDL_SCANCODE_W])
+		BrMatrix34PreTranslate(&prog.camera->t.t.mat, BR_SCALAR(0), BR_SCALAR(0), BR_SCALAR(-8) * BR_SCALAR(dt));
+	if (prog.keys[SDL_SCANCODE_DOWN] || prog.keys[SDL_SCANCODE_S])
+		BrMatrix34PreTranslate(&prog.camera->t.t.mat, BR_SCALAR(0), BR_SCALAR(0), BR_SCALAR(8) * BR_SCALAR(dt));
 
-    /* moving left and right */
-    if (prog.keys[SDL_SCANCODE_A])
-        BrMatrix34PreTranslate(&prog.camera->t.t.mat, BR_SCALAR(-8) * BR_SCALAR(dt), BR_SCALAR(0), BR_SCALAR(0));
-    if (prog.keys[SDL_SCANCODE_D])
-        BrMatrix34PreTranslate(&prog.camera->t.t.mat, BR_SCALAR(8) * BR_SCALAR(dt), BR_SCALAR(0), BR_SCALAR(0));
+	/* moving left and right */
+	if (prog.keys[SDL_SCANCODE_A])
+		BrMatrix34PreTranslate(&prog.camera->t.t.mat, BR_SCALAR(-8) * BR_SCALAR(dt), BR_SCALAR(0), BR_SCALAR(0));
+	if (prog.keys[SDL_SCANCODE_D])
+		BrMatrix34PreTranslate(&prog.camera->t.t.mat, BR_SCALAR(8) * BR_SCALAR(dt), BR_SCALAR(0), BR_SCALAR(0));
 
-    /* moving up and down */
-    if (prog.keys[SDL_SCANCODE_PAGEUP])
-        BrMatrix34PreTranslate(&prog.camera->t.t.mat, BR_SCALAR(0), BR_SCALAR(8) * BR_SCALAR(dt), BR_SCALAR(0));
-    if (prog.keys[SDL_SCANCODE_PAGEDOWN])
-        BrMatrix34PreTranslate(&prog.camera->t.t.mat, BR_SCALAR(0), BR_SCALAR(-8) * BR_SCALAR(dt), BR_SCALAR(0));
+	/* moving up and down */
+	if (prog.keys[SDL_SCANCODE_PAGEUP])
+		BrMatrix34PreTranslate(&prog.camera->t.t.mat, BR_SCALAR(0), BR_SCALAR(8) * BR_SCALAR(dt), BR_SCALAR(0));
+	if (prog.keys[SDL_SCANCODE_PAGEDOWN])
+		BrMatrix34PreTranslate(&prog.camera->t.t.mat, BR_SCALAR(0), BR_SCALAR(-8) * BR_SCALAR(dt), BR_SCALAR(0));
 
-    /* update bbox */
-    BrMatrix34Copy(&prog.camera_bbox->t.t.mat, &prog.camera->t.t.mat);
+	/* update bbox */
+	BrMatrix34Copy(&prog.camera_bbox->t.t.mat, &prog.camera->t.t.mat);
 
-    /* determine collision with any map block */
-    br_actor *block;
-    BR_FOR_SIMPLELIST(prog.level->children, block)
-    {
-        if (actor_overlapping(block, prog.camera_bbox))
-            printf("camera colliding!\n");
-    }
+	/* determine collision with any map block */
+	br_actor *block;
+	BR_FOR_SIMPLELIST(prog.level->children, block)
+	{
+		if (actor_overlapping(block, prog.camera_bbox))
+			printf("camera colliding!\n");
+	}
 
-    /* do render */
-    BrPixelmapFill(prog.colour, BR_COLOUR_RGB(66, 66, 66));
-    BrPixelmapFill(prog.depth, 0xFFFFFFFF);
-    BrZbSceneRender(prog.world, prog.camera, prog.colour, prog.depth);
+	/* do render */
+	BrPixelmapFill(prog.colour, BR_COLOUR_RGB(66, 66, 66));
+	BrPixelmapFill(prog.depth, 0xFFFFFFFF);
+	BrZbSceneRender(prog.world, prog.camera, prog.colour, prog.depth);
 
-    return BRE_OK;
+	return BRE_OK;
 }
 
 br_error prog_event(SDL_Event *event)
 {
-    return BRE_OK;
+	return BRE_OK;
 }
 
 int main(int argc, char **argv)
 {
-    /* set hooks */
-    BrProgramInitHook(prog_init);
-    BrProgramQuitHook(prog_quit);
-    BrProgramRenderHook(prog_render);
-    BrProgramEventHook(prog_event);
+	/* set hooks */
+	BrProgramInitHook(prog_init);
+	BrProgramQuitHook(prog_quit);
+	BrProgramRenderHook(prog_render);
+	BrProgramEventHook(prog_event);
 
-    /* initialize program */
-    BrProgramInit("tile", 1280, 720);
+	/* initialize program */
+	BrProgramInit("tile", 1280, 720);
 
-    /* do main loop */
-    BrProgramLoop();
+	/* do main loop */
+	BrProgramLoop();
 
-    /* quit program */
-    BrProgramQuit();
+	/* quit program */
+	BrProgramQuit();
 
-    return 0;
+	return 0;
 }
