840a63041c3649c99912e9aa3b175c799d3e8c51
Jaycie Ewald <jaycie.ewald@outlook.com>
2023-12-17 16:28:47 -0600

    formats: add rlew module

---
+++ c/formats/rlew.h
@@ -0,0 +1,54 @@
+/*
+MIT License
+
+Copyright (c) 2023 erysdren (it/she/they)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+ * Name: RLEW Format Module
+ *
+ * Description: Support functions for reading and writing RLEW encoded data.
+ *
+ * Dependencies:
+ * - libc
+ */
+
+#pragma once
+#ifndef _RLEW_H_
+#define _RLEW_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+/* compress data from SRC and write into DEST */
+/* returns the number of bytes written to DEST */
+size_t rlew_compress(uint16_t *src, size_t src_len, uint16_t tag, uint16_t *dest, size_t dest_len);
+
+/* uncompress data from SRC and write into DEST */
+/* returns the number of bytes written to DEST */
+size_t rlew_uncompress(uint16_t *src, size_t src_len, uint16_t tag, uint16_t *dest, size_t dest_len);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* _RLEW_H_ */
