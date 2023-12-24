454c7ae20ec6d1eff0846b862a38e57c6947b69b
Jaycie Ewald <jaycie.ewald@outlook.com>
2023-12-23 18:26:58 -0600

    Rename sdlray to dreamray

--- c/dreamray/main.c
+++ c/dreamray/main.c
@@ -221,7 +221,7 @@ int main(int argc, char **argv)
 	SDL_Init(SDL_INIT_VIDEO);
 	IMG_Init(IMG_INIT_PNG);
 
-	sdl.window = SDL_CreateWindow("sdlray",
+	sdl.window = SDL_CreateWindow("dreamray",
 		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 		WIDTH, HEIGHT,
 		SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_MAXIMIZED
