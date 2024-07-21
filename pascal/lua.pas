5acef987dc22bc162913e28e2d30a9ca4acf6dd9
erysdren <contact@erysdren.me>
2024-07-21 11:36:46 -0500

    pascal lua: it works sorta

--- pascal/lua.pas
+++ pascal/lua.pas
@@ -9,31 +9,64 @@ interface
 uses
 	ctypes;
 type
+	lua_State = pointer;
 	LuaState = class
 	private
-		L : pointer;
+		L: lua_State;
 	public
+		function doString(s: AnsiString): integer;
+		procedure openLibs();
 		constructor create();
 		destructor destroy();
 	end;
 
-{ c lua functions }
-function luaL_newstate() : pointer; cdecl; external 'lua';
-procedure lua_close(L : pointer); cdecl; external 'lua';
+{ lua.h }
+procedure lua_close(L: lua_State); cdecl; external 'lua';
+function lua_pcallk(L: lua_State; nargs: integer; nresults: integer; errfunc: integer; ctx: pointer; k: pointer): integer; cdecl; external 'lua';
+
+{ lualib.h }
+function luaopen_base(L: lua_State): integer; cdecl; external 'lua';
+function luaopen_coroutine(L: lua_State): integer; cdecl; external 'lua';
+function luaopen_table(L: lua_State): integer; cdecl; external 'lua';
+function luaopen_io(L: lua_State): integer; cdecl; external 'lua';
+function luaopen_os(L: lua_State): integer; cdecl; external 'lua';
+function luaopen_string(L: lua_State): integer; cdecl; external 'lua';
+function luaopen_utf8(L: lua_State): integer; cdecl; external 'lua';
+function luaopen_math(L: lua_State): integer; cdecl; external 'lua';
+function luaopen_debug(L: lua_State): integer; cdecl; external 'lua';
+function luaopen_package(L: lua_State): integer; cdecl; external 'lua';
+procedure luaL_openlibs(L: lua_State); cdecl; external 'lua';
+
+{ lauxlib.h }
+function luaL_newstate(): lua_State; cdecl; external 'lua';
+function luaL_loadstring(L: lua_State; s: pchar): integer; cdecl; external 'lua';
 
 { LuaState class implementation }
 implementation
 
 constructor LuaState.create();
 begin
-	writeLn('hello world!');
 	L := luaL_newstate();
 end;
 
 destructor LuaState.destroy();
 begin
 	lua_close(L);
-	writeLn('goodbye world!');
+end;
+
+function LuaState.doString(s: AnsiString): integer;
+var
+	ret : integer;
+begin
+	ret := luaL_loadstring(L, pansichar(s));
+	if (ret = 0) then
+		ret := lua_pcallk(L, 0, -1, 0, nil, nil);
+	doString := ret;
+end;
+
+procedure LuaState.openLibs();
+begin
+	luaL_openlibs(L);
 end;
 
 end.
--- pascal/lua_test.pas
+++ pascal/lua_test.pas
@@ -2,11 +2,16 @@ program LuaTest;
 
 uses
 	Lua;
-
+const
+	lua_script = 'print("hello world!\n")';
 var
 	L : LuaState;
+	ret : integer;
 
 begin
 	L := LuaState.create();
+	L.openLibs();
+	ret := L.doString(lua_script);
+	writeLn(ret);
 	L.destroy();
 end.
